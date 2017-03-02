55f47180e173a53501c5ac5dd6ec4da093a8e4b7
Denis Valcke <denis.valcke@gmail.com>
2017-03-02 16:06:33 +0100

    compiled component

--- lib/dynamic-load.component.d.ts
+++ lib/dynamic-load.component.d.ts
@@ -1,10 +1,11 @@
-import { ViewContainerRef, ComponentFactoryResolver, OnChanges, OnDestroy } from '@angular/core';
+import { OnChanges, OnDestroy } from '@angular/core';
 export declare class DynamicLoadComponent implements OnChanges, OnDestroy {
     private vcr;
     private cfr;
+    private dynamicLoadService;
     componentData: any;
     private currentComponent;
-    constructor(vcr: ViewContainerRef, cfr: ComponentFactoryResolver);
-    ngOnChanges(): void;
+    constructor(vcr: any, cfr: any, dynamicLoadService: any);
+    ngOnChanges(): boolean;
     ngOnDestroy(): void;
 }
