ee7bb7f31ac8682419fb57a61b74d7c659348ff0
Denis Valcke <denis.valcke@gmail.com>
2017-03-03 08:49:01 +0100

    added files to the tsconfig.json file & created an index.ts + transpile it with gulp like the src

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,13 +5,20 @@ var merge = require('merge2');
 var tsConfig = require('./tsconfig.json');
 
 function transpile() {
-    var tResult = gulp.src(['./src/**/*.ts'])
+    var indexResult = gulp.src(['./index.ts'])
+        .pipe(embed())
+        .pipe(ts(tsConfig.compilerOptions));
+
+    var libResult = gulp.src(['./src/**/*.ts'])
         .pipe(embed())
         .pipe(ts(tsConfig.compilerOptions));
 
     return merge([
-        tResult.dts.pipe(gulp.dest('./lib')),
-        tResult.js.pipe(gulp.dest('./lib'))
+        indexResult.dts.pipe(gulp.dest('./')),
+        indexResult.js.pipe(gulp.dest('./')),
+
+        libResult.dts.pipe(gulp.dest('./lib')),
+        libResult.js.pipe(gulp.dest('./lib'))
     ]);
 }
 
--- templating-demo/src/app/app.module.ts
+++ templating-demo/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { DynamicLoadModule } from 'wcm-template-manager-ng2';
+// import { DynamicLoadModule } from 'wcm-template-manager-ng2';
+import { DynamicLoadModule } from './dynamic/dynamic-load.module';
 import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '@angular/material';
