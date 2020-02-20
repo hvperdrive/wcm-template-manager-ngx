7d2fb7bd318517b14c6ec354c56d5375372d6c1d
Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>
2020-02-20 17:33:55 +0100

    chore: cleanup old codebase

--- lib/src/dynamic-load.module.ngfactory.d.ts
+++
@@ -1,3 +0,0 @@
-import * as i0 from '@angular/core';
-import * as i1 from './dynamic-load.module';
-export declare const DynamicLoadModuleNgFactory: i0.NgModuleFactory<i1.DynamicLoadModule>;
--- lib/src/dynamic-load.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class DynamicLoadService {
-    private components;
-    constructor(components: any);
-    getViewComponents(): any;
-    getPartialComponents(): any;
-    getContentComponents(): any;
-    selectComponent(type: string, data: any): boolean;
-    private _findComponent;
-}
